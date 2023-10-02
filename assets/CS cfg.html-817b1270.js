15b128d54fa3a6a4bdb5c118252b579baab24067
nxy1999 <37256108+nxy1999@users.noreply.github.com>
2023-10-02 14:04:21 +0000

    Deploying to gh-pages from @ nxy1999/nxy1999.github.io@4a012c52b76fe19a877da00423fd7ee24a9b4920 🚀

---
+++ assets/CS cfg.html-817b1270.js
@@ -0,0 +1,32 @@
+import{_ as s}from"./plugin-vue_export-helper-c27b6911.js";import{r as c,o as l,c as r,a as e,b as n,d as a,e as d}from"./app-5da354f1.js";const o={},v=e("h2",{id:"cs-config-cfg-文件内容记录",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#cs-config-cfg-文件内容记录","aria-hidden":"true"},"#"),n(" CS config(.cfg)文件内容记录")],-1),t=e("h3",{id:"准星代码",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#准星代码","aria-hidden":"true"},"#"),n(" 准星代码")],-1),m={href:"https://totalcsgo.com/commands",target:"_blank",rel:"noopener noreferrer"},u=d(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>cl_crosshair_drawoutline 0; // 准星外围的边线 （0 不显示）
+
+cl_crosshairalpha 255; // 准星透明度 （255 不透明 0 全透）
+
+// cl_crosshaircolor 1; // 我在游戏设置界面没有找到，注释掉用下面的RGB代替
+// 0 红 1 绿 2 黄 3 深蓝 4 浅蓝色(青色)
+
+cl_crosshaircolor_b 255; // 蓝色的色值（0-255）
+
+cl_crosshaircolor_g 0; // 绿色的色值（0-255）
+
+cl_crosshaircolor_r 0; // 红色的色值 （0-255）
+
+cl_crosshairdot 0; // 中心点显示（0 不显示 1 显示）
+
+cl_crosshairgap -3; // 准星之间的空隙大小
+
+cl_crosshairsize 2; // 准星长度
+
+cl_crosshairstyle 4; // 准星类型 4是经典静态
+
+// 0 - 默认的 CS:GO 动态十字准线
+// 1 - 默认十字准线的静态变体
+// 2 - 经典静态十字准线，两侧各有一个动态圆点
+// 3 - 经典动态十字准线
+// 4 - 经典静态十字准线
+// 5 - 经典静态和动态十字准线
+
+cl_crosshairthickness 0; // 准星粗细
+
+cl_crosshair_sniper_width 1; // 此命令设置使用狙击步枪瞄准镜时十字准线宽度的宽度（以像素为单位）。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1);function _(b,h){const i=c("ExternalLinkIcon");return l(),r("div",null,[v,t,e("p",null,[n("一个查询代码含义的网站："),e("a",m,[n("https://totalcsgo.com/commands"),a(i)])]),u])}const p=s(o,[["render",_],["__file","CS cfg.html.vue"]]);export{p as default};
